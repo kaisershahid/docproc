4b164f9fa58cac4a446d208e54da6760445d151b
kaiser shahid <kaisershahid@gmail.com>
2020-09-24 00:06:50 -0700

    -- hashed headers complete!!!

--- src/plugins/markdown/header.test.ts
+++ src/plugins/markdown/header.test.ts
@@ -8,49 +8,34 @@ import { Lexer } from "../../lexer";
 import { CodeHandler } from "./code";
 import { ParagraphHandler } from "./paragraph";
 import { HorizontalRuleHandler } from "./horizontal-rule";
+import { HeaderHandler } from "./header";
 
-describe("plugins.markdown.code", () => {
+describe.only("plugins.markdown.code", () => {
   const subject = new ListHandler();
   const blockManager = new HandlerManager<BlockHandlerType>();
   blockManager.addHandler(subject);
   blockManager.addHandler(new ParagraphHandler());
-  blockManager.addHandler(new HorizontalRuleHandler());
+  blockManager.addHandler(new HeaderHandler());
   const lexer = new Lexer();
   addToLexer(lexer, true);
   const docprocBase = new DocProcessor({ blockManager, lexer });
 
-  it("handles ---", () => {
+  it("handles # header1", () => {
     const docproc = new DocProcessor(docprocBase.makeContext());
-    docproc.process("---");
+    docproc.process("# header1");
     const html = docproc.toString();
-    expect(html).to.equal(`<hr />`);
+    expect(html).to.equal(`<h1> header1</h1>`);
   });
-
-  it("handles ****", () => {
+  it("handles # header1\\n###### header6", () => {
     const docproc = new DocProcessor(docprocBase.makeContext());
-    docproc.process("****");
+    docproc.process("# header1\n###### header6");
     const html = docproc.toString();
-    expect(html).to.equal(`<hr />`);
+    expect(html).to.equal(`<h1> header1</h1>\n<h6> header6</h6>`);
   });
-
-  it("handles 2 rows of ****", () => {
+  it("handles # header1\\nnew paragraph", () => {
     const docproc = new DocProcessor(docprocBase.makeContext());
-    docproc.process("****\n****");
+    docproc.process("# header1\nnew paragraph");
     const html = docproc.toString();
-    expect(html).to.equal(`<hr />\n<hr />`);
-  });
-
-  it("handles ---- abc...", () => {
-    const docproc = new DocProcessor(docprocBase.makeContext());
-    docproc.process("---- abc...");
-    const html = docproc.toString();
-    expect(html).to.equal(`<hr />`);
-  });
-
-  it("handles ----\\nabc...", () => {
-    const docproc = new DocProcessor(docprocBase.makeContext());
-    docproc.process("----\nabc...");
-    const html = docproc.toString();
-    expect(html).to.equal(`<hr />\n<p>abc...</p>`);
+    expect(html).to.equal(`<h1> header1</h1>\n<p>new paragraph</p>`);
   });
 });
---
+++ src/plugins/markdown/header.ts
@@ -0,0 +1,92 @@
+import {
+  BlockActions,
+  BlockHandlerType,
+  DocContext,
+  HandlerInterface,
+  InlineFormatterInterface,
+  LexemeDef,
+} from "../../types";
+import { isLineEnd } from "../../utils";
+import { BlockBase } from "../../defaults/block-base";
+
+export const REGEX_HEADER_START = /^#{1,6}/;
+
+export class Header {
+  level: number;
+  formatter: InlineFormatterInterface;
+
+  constructor(level: number, context: DocContext) {
+    // @todo use context to register this header for TOC (through some dispatching mechanism)
+    this.level = level;
+    this.formatter = context.getInlineFormatter();
+  }
+
+  push(lexeme: string, def?: LexemeDef) {
+    this.formatter.push(lexeme, def);
+  }
+
+  toString() {
+    return `<h${this.level}>${this.formatter.toString()}</h${this.level}>`;
+  }
+}
+
+/**
+ * Handle headers <h1/> ... <h6/>
+ */
+export class HeaderHandler extends BlockBase {
+  getName() {
+    return "header";
+  }
+
+  canAccept(lexeme: string, def?: LexemeDef) {
+    return REGEX_HEADER_START.test(lexeme);
+  }
+
+  lastLex = "";
+  headers: Header[] = [];
+
+  curHeader(): Header {
+    const idx = this.headers.length - 1;
+    const header = this.headers[idx];
+    if (!header) {
+      throw new Error("no current header");
+    }
+    return header;
+  }
+
+  push(lexeme: string, def?: LexemeDef): BlockActions {
+    const lastLineEnd = isLineEnd(this.lastLex);
+    const lastLex = this.lastLex;
+    this.lastLex = lexeme;
+    if (REGEX_HEADER_START.test(lexeme)) {
+      return this.pushHeaderStart(lexeme, lastLex);
+    } else if (lastLineEnd) {
+      if (isLineEnd(lexeme)) {
+        return BlockActions.DONE;
+      }
+      return BlockActions.REJECT;
+    } else if (!isLineEnd(lexeme)) {
+      this.curHeader().push(lexeme, def);
+      return BlockActions.CONTINUE;
+    }
+
+    return BlockActions.CONTINUE;
+  }
+
+  protected pushHeaderStart(lexeme: string, lastLex: string): BlockActions {
+    if (lastLex == "" || isLineEnd(lastLex)) {
+      const level = lexeme.length;
+      this.headers.push(new Header(level, this.context as DocContext));
+    }
+
+    return BlockActions.CONTINUE;
+  }
+
+  cloneInstance(): HandlerInterface<BlockHandlerType> {
+    return new HeaderHandler();
+  }
+
+  toString() {
+    return this.headers.join("\n");
+  }
+}
