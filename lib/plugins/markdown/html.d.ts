2c4a30cd3fd1ca95dde65bd70e5cb284b57ef77b
kaiser shahid <kaisershahid@gmail.com>
2020-10-04 08:26:16 -0700

    -- tsc build for general enhancements

--- lib/plugins/markdown/html.d.ts
+++ lib/plugins/markdown/html.d.ts
@@ -1,5 +1,5 @@
 import { BlockBase } from "../../defaults/block-base";
-import { BlockHandlerType, HandlerInterface, LexemeDef } from "../../types";
+import { BlockHandlerType, HandlerInterface, LexemeConsumer, LexemeDef } from "../../types";
 export declare enum EnclosingTagState {
     start = 0,
     tag_starting = 1,
@@ -8,10 +8,18 @@ export declare enum EnclosingTagState {
     tag_closed = 4
 }
 /**
- * Detects block-level tags and treats its content as a single block. For LiteralBodyTags,
- * all content is passed through as-is.
+ * Detects and treats specific HTML tags as block elements, such that all content within the same tag is treated as a
+ * single block, with some caveats:
+ *
+ * - For tags in {@see LiteralBodyTags} like `<style/>`, tag content is passed through
+ * - For tags in {@see ContainerTags} like `<body/>`, content is treated as a sub-document, so that markdown block
+ *   processing continues to apply
+ * - otherwise, content is treated like inline text.
  */
 export declare class HtmlBlockHandler extends BlockBase implements HandlerInterface<BlockHandlerType> {
+    pusher: LexemeConsumer;
+    closer: () => void;
+    constructor();
     /**
      * Only handle tags defined in BlockTags.
      * @param lexeme
@@ -25,10 +33,16 @@ export declare class HtmlBlockHandler extends BlockBase implements HandlerInterf
     state: EnclosingTagState;
     buff: any[];
     push(lexeme: string, def: LexemeDef | undefined): any;
+    isContainer: boolean;
     tagName: string;
+    tagOpenStart: string;
     tagCloseStart: string;
+    tagsOpen: number;
     private handleStart;
     private handleTagStarting;
+    protected setToContainerPush(): void;
+    protected setToPassthroughPush(): void;
+    protected setToInlinePush(): void;
     private handleTagOpen;
     private handleTagClosing;
     private handleTagClosed;
