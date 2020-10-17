8214b9e2e705a044afc4b238f63231c21cdff414
kaiser shahid <kaisershahid@gmail.com>
2020-10-17 11:07:49 -0700

    *npm run build*

--- lib/defaults/block-base.d.ts
+++ lib/defaults/block-base.d.ts
@@ -1,6 +1,13 @@
-import { ContextAwareInterface, DocProcContext, InlineFormatterInterface } from "../types";
+import { BlockActions, BlockHandlerType, ContextAwareInterface, DocProcContext, HandlerInterface, InlineFormatterInterface, LexemeDef } from "../types";
 export declare class BlockBase implements ContextAwareInterface {
     protected context?: DocProcContext;
     protected inlineFormatter: InlineFormatterInterface;
     setContext(context: DocProcContext): void;
+}
+export declare class BlockEmptyHandler extends BlockBase implements HandlerInterface<BlockHandlerType> {
+    getName(): string;
+    canAccept(lexeme: string, def?: LexemeDef): boolean;
+    push(lexeme: string, def?: LexemeDef): BlockActions;
+    cloneInstance(): HandlerInterface<BlockHandlerType>;
+    toString(): void;
 }
